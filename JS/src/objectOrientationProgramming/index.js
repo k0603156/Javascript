42c73d934c4ffcdb52afe52ef6b7c659699542f2
kimyongkuk <k0603156@gmail.com>
2021-08-22 20:29:08 +0900

    feat: Observer

--- JS/src/objectOrientationProgramming/index.js
+++ JS/src/objectOrientationProgramming/index.js
@@ -1,57 +1,18 @@
-const { type, ViewModel, BinderItem, Binder, Processor, Scanner } = require("./util.js");
+const {
+	ViewModel,
+	Processor,
+	Scanner,
+} = require("./util.js");
 
 (() => {
-	const viewmodel = ViewModel.get({
-		changeContents() {
-			this.wrapper.styles.background = `rgb(
-                    ${parseInt(Math.random() * 150) + 100},
-                    ${parseInt(Math.random() * 150) + 100},
-                    ${parseInt(Math.random() * 150) + 100}
-                )`;
-			this.contents.properties.innerHTML = Math.random()
-				.toString(16)
-				.replace(".", "");
-		},
-		wrapper: ViewModel.get({
-			isStop: false,
-			styles: {
-				width: "50%",
-				background: "#ffa",
-				cursor: "pointer",
-			},
-			events: {
-				click(e, vm) {
-					vm.isStop = true;
-				},
-			},
-		}),
-		title: ViewModel.get({
-			properties: {
-				innerHTML: "Title",
-			},
-		}),
-		contents: ViewModel.get({
-			properties: {
-				innerHTML: "contents",
-			},
-		}),
-	});
 	const scanner = new Scanner();
 	const binder = scanner.scan(document.querySelector("#target"));
-
 	binder.addProcessor(
 		new (class extends Processor {
 			_process(vm, el, k, v) {
 				el.style[k] = v;
 			}
 		})("styles"),
-	);
-	binder.addProcessor(
-		new (class extends Processor {
-			_process(vm, el, k, v) {
-				el[k] = v;
-			}
-		})("properties"),
 	);
 	binder.addProcessor(
 		new (class extends Processor {
@@ -63,15 +24,70 @@ const { type, ViewModel, BinderItem, Binder, Processor, Scanner } = require("./u
 	binder.addProcessor(
 		new (class extends Processor {
 			_process(vm, el, k, v) {
+				el[k] = v;
+			}
+		})("properties"),
+	);
+	binder.addProcessor(
+		new (class extends Processor {
+			_process(vm, el, k, v) {
 				el["on" + k] = (e) => v.call(el, e, vm);
 			}
 		})("events"),
 	);
-
+	const viewmodel = ViewModel.get({
+		isStop: false,
+		changeContents() {
+			this.wrapper.styles.background = `rgb(${
+				parseInt(Math.random() * 150) + 100
+			},${parseInt(Math.random() * 150) + 100},${
+				parseInt(Math.random() * 150) + 100
+			})`;
+			this.contents.properties.innerHTML = Math.random()
+				.toString(16)
+				.replace(".", "");
+		},
+		wrapper: ViewModel.get({
+			styles: {
+				width: "50%",
+				background: "#ffa",
+				cursor: "pointer",
+			},
+			events: {
+				click(e, vm) {
+					vm.parent.isStop = true;
+				},
+			},
+		}),
+		title: ViewModel.get({
+			properties: {
+				innerHTML: "Title",
+			},
+		}),
+		contents: ViewModel.get({
+			properties: {
+				innerHTML: "Contents",
+			},
+		}),
+		input: ViewModel.get({
+			properties: {
+				value: "Title",
+			},
+			events: {
+				input(e, vm) {
+					if (e.isComposing) return;
+					vm.properties.value = e.target.value;
+				},
+				blur(e, vm) {
+					vm.properties.value = e.target.value.trim();
+				},
+			},
+		}),
+	});
+	binder.watch(viewmodel);
 	const f = (_) => {
 		viewmodel.changeContents();
-		binder.render(viewmodel);
-		if (!viewmodel.wrapper.isStop) requestAnimationFrame(f);
+		if (!viewmodel.isStop) requestAnimationFrame(f);
 	};
 	requestAnimationFrame(f);
 })();
--- JS/src/objectOrientationProgramming/util.js
+++ JS/src/objectOrientationProgramming/util.js
@@ -6,100 +6,110 @@ const type = (target, type) => {
 		throw TypeError(`invaild type ${target} : ${type}`);
 	return target;
 };
+const ViewModelListener = class {
+	viewmodelUpdated(target, updated) {
+		throw "override";
+	}
+};
+const ViewModelValue = class {
+	subKey;
+	cat;
+	k;
+	v;
+	constructor(subKey, cat, k, v) {
+		this.subKey = subKey;
+		this.cat = cat;
+		this.k = k;
+		this.v = v;
+		Object.freeze(this);
+	}
+};
 
-const ViewModel = class {
-	static #private = Symbol();
+const ViewModel = class extends ViewModelListener {
 	static get(data) {
-		return new ViewModel(this.#private, data);
+		return new ViewModel(data);
+	}
+	static #subjects = new Set();
+	static #inited = false;
+	static notify(vm) {
+		this.#subjects.add(vm);
+		if (this.#inited) return;
+		this.#inited = true;
+		const f = (_) => {
+			this.#subjects.forEach((vm) => {
+				if (vm.#isUpdated.size) {
+					vm.notify();
+					vm.#isUpdated.clear();
+				}
+			});
+			requestAnimationFrame(f);
+		};
+		requestAnimationFrame(f);
+	}
+	static define(vm, cat, obj) {
+		return Object.defineProperties(
+			obj,
+			Object.entries(obj).reduce((r, [k, v]) => {
+				r[k] = {
+					enumerable: true,
+					get: (_) => v,
+					set: (newV) => {
+						v = newV;
+						vm.#isUpdated.add(new ViewModelValue(vm.subKey, cat, k, v));
+					},
+				};
+				return r;
+			}, {}),
+		);
 	}
 	styles = {};
 	attributes = {};
 	properties = {};
 	events = {};
-	constructor(checker, data) {
-		if (checker != ViewModel.#private) throw "use ViewModel.get()";
+	subKey = "";
+	parent = null;
+	#isUpdated = new Set();
+	#listeners = new Set();
+	constructor(data, _ = type(data, "object")) {
+		super();
 		Object.entries(data).forEach(([k, v]) => {
-			switch (k) {
-				case "styles":
-					this.styles = v;
-					break;
-				case "attributes":
-					this.attributes = v;
-					break;
-				case "properties":
-					this.properties = v;
-					break;
-				case "events":
-					this.events = v;
-					break;
-				default:
-					this[k] = v;
+			if ("styles,attributes,properties".includes(k)) {
+				if (!v || typeof v != "object") throw `invalid object k:${k}, v:${v}`;
+				this[k] = ViewModel.define(this, k, v);
+			} else {
+				Object.defineProperty(this, k, {
+					enumerable: true,
+					get: (_) => v,
+					set: (newV) => {
+						v = newV;
+						this.#isUpdated.add(new ViewModelValue(this.subKey, "", k, v));
+					},
+				});
+				if (v instanceof ViewModel) {
+					v.parent = this;
+					v.subKey = k;
+					v.addListener(this);
+				}
 			}
 		});
+		ViewModel.notify(this);
 		Object.seal(this);
 	}
-};
-
-const BinderItem = class {
-	el;
-	viewmodel;
-	constructor(
-		el,
-		viewmodel,
-		_0 = type(el, HTMLElement),
-		_1 = type(viewmodel, "string"),
-	) {
-		this.el = el;
-		this.viewmodel = viewmodel;
-		Object.freeze(this);
+	viewmodelUpdated(target, updated) {
+		updated.forEach((v) => this.#isUpdated.add(v));
+	}
+	addListener(v, _ = type(v, ViewModelListener)) {
+		this.#listeners.add(v);
+	}
+	removeListener(v, _ = type(v, ViewModelListener)) {
+		this.#listeners.delete(v);
+	}
+	notify() {
+		this.#listeners.forEach((v) =>
+			v.viewmodelUpdated(this, this.#isUpdated),
+		);
 	}
 };
-
-const Binder = class {
-	#items = new Set();
-	#processors = {};
-	add(v, _ = type(v, BinderItem)) {
-		this.#items.add(v);
-	}
-	addProcessor(v, _0 = type(v, Processor)) {
-		this.#processors[v.cat] = v;
-	}
-	render(viewmodel, _ = type(viewmodel, ViewModel)) {
-		const processores = Object.entries(this.#processors);
-		this.#items.forEach((item) => {
-			const vm = type(viewmodel[item.viewmodel], ViewModel), el = item.el;
-			processores.forEach(([pk, processor]) => {
-				Object.entries(vm[pk]).forEach(([k, v]) => {
-					processor.process(vm, el, k, v);
-				});
-			});
-		});
-	}
-};
-
-const Processor = class {
-	cat;
-	constructor(cat) {
-		this.cat = cat;
-		Object.freeze(this);
-	}
-	z;
-	process(
-		vm,
-		el,
-		k,
-		v,
-		_0 = type(vm, ViewModel),
-		_1 = type(el, HTMLElement),
-		_2 = type(k, "string"),
-	) {
-		this._process(vm, el, k, v);
-	}
-	_process(vm, el, k, v) {
-		throw "override";
-	}
-};
-
 const Scanner = class {
 	scan(el, _ = type(el, HTMLElement)) {
 		const binder = new Binder();
@@ -116,6 +126,87 @@ const Scanner = class {
 	checkItem(binder, el) {
 		const vm = el.getAttribute("data-viewmodel");
 		if (vm) binder.add(new BinderItem(el, vm));
+	}
+};
+const Processor = class {
+	cat;
+	constructor(cat) {
+		this.cat = cat;
+		Object.freeze(this);
+	}
+	process(
+		vm,
+		el,
+		k,
+		v,
+		_0 = type(vm, ViewModel),
+		_1 = type(el, HTMLElement),
+		_2 = type(k, "string"),
+	) {
+		this._process(vm, el, k, v);
+	}
+	_process(vm, el, k, v) {
+		throw "override";
+	}
+};
+
+const Binder = class extends ViewModelListener {
+	#items = new Set();
+	#processors = {};
+	add(v, _ = type(v, BinderItem)) {
+		this.#items.add(v);
+	}
+	viewmodelUpdated(target, updated) {
+		const items = {};
+		this.#items.forEach((item) => {
+			items[item.viewmodel] = [
+				type(target[item.viewmodel], ViewModel),
+				item.el,
+			];
+		});
+		updated.forEach((v) => {
+			if (!items[v.subKey]) return;
+			const [vm, el] = items[v.subKey],
+				processor = this.#processors[v.cat];
+			if (!el || !processor) return;
+			processor.process(vm, el, v.k, v.v);
+		});
+	}
+	addProcessor(v, _0 = type(v, Processor)) {
+		this.#processors[v.cat] = v;
+	}
+	watch(viewmodel, _ = type(viewmodel, ViewModel)) {
+		viewmodel.addListener(this);
+		this.render(viewmodel);
+	}
+	unwatch(viewmodel, _ = type(viewmodel, ViewModel)) {
+		viewmodel.removeListener(this);
+	}
+	render(viewmodel, _ = type(viewmodel, ViewModel)) {
+		const processores = Object.entries(this.#processors);
+		this.#items.forEach((item) => {
+			const vm = type(viewmodel[item.viewmodel], ViewModel),
+				el = item.el;
+			processores.forEach(([pk, processor]) => {
+				Object.entries(vm[pk]).forEach(([k, v]) => {
+					processor.process(vm, el, k, v);
+				});
+			});
+		});
+	}
+};
+const BinderItem = class {
+	el;
+	viewmodel;
+	constructor(
+		el,
+		viewmodel,
+		_0 = type(el, HTMLElement),
+		_1 = type(viewmodel, "string"),
+	) {
+		this.el = el;
+		this.viewmodel = viewmodel;
+		Object.freeze(this);
 	}
 };
 
